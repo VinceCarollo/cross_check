3253d66317438a3347c0d25115057aab2495c2d1
Andrew Johnson <manilda7@gmail.com>
2019-04-01 16:25:22 -0600

    Add setup and first test

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,7 @@
+# Create a class method call from_csv
+class StatTracker
+
+  def self.from_csv(locations)
+    
+  end
+end
---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/stat_tracker'
+
+class StatTrackerTest < Minitest::Test
+
+  def setup
+    game_path = './data/game.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+  def test_stat_tracker_exists
+    assert StatTracker, @stat_tracker
+  end
+end
