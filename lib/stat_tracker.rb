d714d442767b49539c9260b5dbbaa551039db37f
Vince Carollo <vincecarollo@gmail.com>
2019-04-03 17:12:48 -0600

    Sets up stat tracker to work with game stats module
    Co-authored-by: Andrew <manilda7@gmail.com>
    Co-authored-by: Mills <45856797+MillsProvosty@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,6 +3,7 @@ require_relative './breakdown'
 require_relative './game_stats'
 
 class StatTracker
+  include GameStats
 
   attr_reader :games,
               :teams,
@@ -31,40 +32,4 @@ class StatTracker
     end
     StatTracker.new(games, teams, game_teams)
   end
-
-  def highest_total_score
-    stats = GameStats.new(self.games)
-    stats.highest_total_score
-  end
-
-  def lowest_total_score
-    stats = GameStats.new(self.games)
-    stats.lowest_total_score
-  end
-
-  def biggest_blowout
-    stats = GameStats.new(self.games)
-    stats.biggest_blowout
-  end
-
-  def percentage_home_wins(home_team_id)
-    stats = GameStats.new(self.games)
-    stats.percentage_home_wins(home_team_id)
-  end
-
-  def percentage_visitor_wins(away_team_id)
-    stats = GameStats.new(self.games)
-    stats.percentage_visitor_wins(away_team_id)
-  end
-
-  def count_of_games_by_season(season)
-    stats = GameStats.new(self.games)
-    stats.count_of_games_by_season(season)
-  end
-
-  def average_goals_per_game
-    stats = GameStats.new(self.games)
-    stats.average_goals_per_game
-  end
-
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -15,4 +15,35 @@ class StatTrackerTest < Minitest::Test
   def test_stat_tracker_exists
     assert_instance_of StatTracker, @stat_tracker
   end
+
+  def test_highest_total_score
+    assert_equal 9, @stat_tracker.highest_total_score
+  end
+
+  def test_lowest_total_score
+    assert_equal 2, @stat_tracker.lowest_total_score
+  end
+
+  def test_biggest_blowout
+    assert_equal 5, @stat_tracker.biggest_blowout
+  end
+
+  def test_percentage_home_wins
+    assert_equal 33.33, @stat_tracker.percentage_home_wins("26")
+  end
+
+  def test_percentage_visitor_wins
+    assert_equal 50.0, @stat_tracker.percentage_visitor_wins("14")
+  end
+
+  def test_count_of_games_by_season
+    expected = {
+      "20122013" => 6
+    }
+    assert_equal expected, @stat_tracker.count_of_games_by_season("20122013")
+  end
+
+  def test_average_goals_per_game
+    assert_equal 5.37, @stat_tracker.average_goals_per_game
+  end
 end
