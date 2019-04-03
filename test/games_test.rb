887b24787b5c65658c35814375683a01bad40ba6
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-02 20:19:30 -0600

    Adds tests, more tests needed
    Co-authored-by: Vince <vincecarollo@gmail.com>
    Co-authored-by: Andrew <manilda7@gmail.com>

--- test/games_test.rb
+++ test/games_test.rb
@@ -9,17 +9,21 @@ require './lib/stat_tracker_dummy_initiator'
 class GamesTest < Minitest::Test
   def setup
     @stat_tracker = StatTrackerDummyInitiator.create
-    @games = Games.new(@stat_tracker.games)
+
   end
 
   def test_games_exisist
-    assert_instance_of Games, @games
+    @stat_tracker.game_teams.each do |game|
+      assert_instance_of Games, game
+    end
   end
 
-  def test_methods_return_correct_columns
-    assert_equal "P", @games.type.first
-    expected = ["2", "2", "2", "3", "2", "1", "1", "4", "2", "3", "3", "0", "2", "3", "7", "4", "5", "1", "3"]
-    assert_equal expected, @games.away_goals
-    assert_equal "America/New_York", @games.venue_time_zone_id.first
+  def test_games_has_attributes
+    assert_equal "2012030221" , @stat_tracker.game_teams[0].game_id
+    assert_equal "3", @stat_tracker.game_teams[0].season
+    assert_equal "away", @stat_tracker.game_teams[0].type
+    assert_equal "FALSE", @stat_tracker.game_teams[0].date_time
   end
+
+
 end
