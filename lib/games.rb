7615cd99560e0e35896376256d81c738b9366d64
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-02 20:15:29 -0600

    Corrects methods that call on index numbers rather than hash keys for game class
    Co-authored-by: Vince <vincecarollo@gmail.com>
    Co-authored-by: Andrew <manilda7@gmail.com>

--- lib/games.rb
+++ lib/games.rb
@@ -15,21 +15,23 @@ class Games
               :venue_time_zone_offset,
               :venue_time_zone_tz
 
-  def initialize(stats)
-    @game_id                = stats[:game_id]
-    @season                 = stats[:season]
-    @type                   = stats[:type]
-    @date_time              = stats[:date_time]
-    @away_team_id           = stats[:away_team_id]
-    @home_team_id           = stats[:home_team_id]
-    @away_goals             = stats[:away_goals]
-    @home_goals             = stats[:home_goals]
-    @outcome                = stats[:outcome]
-    @home_rink_side_start   = stats[:home_rink_side_start]
-    @venue                  = stats[:venue]
-    @venue_link             = stats[:venue_link]
-    @venue_time_zone_id     = stats[:venue_time_zone_id]
-    @venue_time_zone_offset = stats[:venue_time_zone_offset]
-    @venue_time_zone_tz     = stats[:venue_time_zone_tz]
+  def initialize(row)
+    @game_id                = row[0]
+    @season                 = row[1]
+    @type                   = row[2]
+    @date_time              = row[3]
+    @away_team_id           = row[4]
+    @home_team_id           = row[5]
+    @away_goals             = row[6]
+    @home_goals             = row[7]
+    @outcome                = row[8]
+    @home_rink_side_start   = row[9]
+    @venue                  = row[10]
+    @venue_link             = row[11]
+    @venue_time_zone_id     = row[12]
+    @venue_time_zone_offset = row[13]
+    @venue_time_zone_tz     = row[14]
   end
+
+
 end
