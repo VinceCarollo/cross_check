61c1108864c5851d92a7c11348b6abecf8b578d7
Vince Carollo <vincecarollo@gmail.com>
2019-04-03 11:19:55 -0600

    Adds testing for most methods in iteration 2
    Co-authored-by: Andrew <manilda7@gmail.com>
    Co-authored-by: Mills <45856797+MillsProvosty@users.noreply.github.com>

--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -30,4 +30,19 @@ class GameStatsTest < Minitest::Test
   def test_biggest_blowout
     assert_equal 5, @game_stats.biggest_blowout
   end
+
+  def test_percentage_home_wins
+    assert_equal 33.33, @game_stats.percentage_home_wins("26")
+  end
+
+  def test_percentage_visitor_wins
+    assert_equal 50.0, @game_stats.percentage_visitor_wins("14")
+  end
+
+  def test_count_of_games_by_season
+    expected = {
+      "20122013" => 6
+    }
+    assert_equal expected, @game_stats.count_of_games_by_season("20122013")
+  end
 end
