598e9ff16124342d7d3f3623ce2f7ac522f9b2b6
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-03 09:44:57 -0600

    Fixes game_test malfunction

--- test/games_test.rb
+++ test/games_test.rb
@@ -9,7 +9,7 @@ require './lib/stat_tracker_dummy_initiator'
 class GamesTest < Minitest::Test
   def setup
     @stat_tracker = StatTrackerDummyInitiator.create
-    @games = GameTeams.new(@stat_tracker.games)
+    @games = Games.new(@stat_tracker.games)
   end
 
   def test_game_teams_exist
