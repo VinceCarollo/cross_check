3170fd641bf3e1e4440e93566b34bbabf484b46c
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-02 20:18:23 -0600

    Initial commit for game stats test class
    Co-authored-by: Vince <vincecarollo@gmail.com>
    Co-authored-by: Andrew <manilda7@gmail.com>

--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -4,20 +4,24 @@ require 'minitest/pride'
 require './lib/game_stats'
 require './lib/stat_tracker'
 require './lib/games'
-require './lib/stat_tracker_initiator'
+require './lib/stat_tracker_dummy_initiator'
 require 'pry'
 
 
 class GameStatsTest < Minitest::Test
 
   def setup
-    @stat_tracker = StatTrackerInitiator.create
+    @stat_tracker = StatTrackerDummyInitiator.create
     @games = Games.new(@stat_tracker.games)
     @game_stats = GameStats.new(@games)
   end
 
   def test_it_exists
     assert_instance_of GameStats, @game_stats
+  end
+
+  def test_highest_total_score
+    assert_equal 12, @game_stats.highest_total_score
   end
 
 end
