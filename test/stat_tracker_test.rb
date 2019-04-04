b70c1f8ccca95915cee4d6d436a85c780ada7778
Vince Carollo <vincecarollo@gmail.com>
2019-04-04 15:10:28 -0600

    Fixes three game stats methods and adds two new league stats methods
    Co-authored-by: Andrew <manilda7@gmail.com>
    Co-authored-by: Mills <45856797+MillsProvosty@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -29,18 +29,20 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_percentage_home_wins
-    assert_equal 33.33, @stat_tracker.percentage_home_wins("26")
+    assert_equal 0.53, @stat_tracker.percentage_home_wins
   end
 
   def test_percentage_visitor_wins
-    assert_equal 50.0, @stat_tracker.percentage_visitor_wins("14")
+    assert_equal 0.47, @stat_tracker.percentage_visitor_wins
   end
 
   def test_count_of_games_by_season
-    expected = {
-      "20122013" => 6
-    }
-    assert_equal expected, @stat_tracker.count_of_games_by_season("20122013")
+    expected = {"20122013"=>6,
+                "20132014"=>1,
+                "20162017"=>4,
+                "20172018"=>3,
+                "20152016"=>5}
+    assert_equal expected, @stat_tracker.count_of_games_by_season
   end
 
   def test_average_goals_per_game
@@ -63,6 +65,10 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_best_offense
-    assert_equal "Devils", @stat_tracker.best_offense
+    assert_equal "Lightning", @stat_tracker.best_offense
+  end
+
+  def test_worst_offense
+    assert_equal "Sabres", @stat_tracker.worst_offense
   end
 end
