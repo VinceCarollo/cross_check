20ce01d7d7cb3ab50683a11d9f5d456509948eaf
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-04 17:32:10 -0600

    Adds team_info method.

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
 module TeamStats
+
+  def team_info(num_arg)
+    info = {}
+    self.teams.each do |team|
+      if team.team_id == num_arg
+        info = {
+          "abbreviation" => team.abbreviation,
+          "franchise_id" => team.franchise_id,
+          "link" => team.link,
+          "short_name" => team.short_name,
+          "team_id" => team.team_id,
+          "team_name" => team.team_name
+        }
+      end
+
+    end
+  info
+  end
+
 end
