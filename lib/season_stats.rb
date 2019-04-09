54118868cf00a5c64b82533d2c5de338e34ee378
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-09 14:53:36 -0600

    Adds method for winningest coach
     Co-authored-by: Vince <vincecarollo@gmail.com>
    Co-authored-by: Andrew <manilda7@gmail.com>

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,15 +1,21 @@
 require 'pry'
 module SeasonStats
 
-  def biggest_bust(season_id)
+  def ids_with_game_arrays_by_season(season_id)
     teams = ids_with_empty_arrays
     self.games.each do |game|
-        teams.each do |team_id, values|
-          if team_id == game.home_team_id || team_id == game.away_team_id
-            teams[team_id] << game if game.season == season_id
-          end
+      teams.each do |team_id, values|
+        if team_id == game.home_team_id || team_id == game.away_team_id
+          teams[team_id] << game if game.season == season_id
         end
+      end
     end
+    teams
+  end
+
+  def biggest_bust(season_id)
+  teams = ids_with_game_arrays_by_season(season_id)
+
     teams.transform_values! do |games|
       games.group_by do |game|
         game.type
@@ -50,14 +56,7 @@ module SeasonStats
   end
 
   def biggest_surprise(season_id)
-    teams = ids_with_empty_arrays
-    self.games.each do |game|
-        teams.each do |team_id, values|
-          if team_id == game.home_team_id || team_id == game.away_team_id
-            teams[team_id] << game if game.season == season_id
-          end
-        end
-    end
+    teams = ids_with_game_arrays_by_season(season_id)
     teams.transform_values! do |games|
       games.group_by do |game|
         game.type
@@ -96,5 +95,40 @@ module SeasonStats
     end
 
     find_team_name(duh_sabres[0])
+  end
+
+  def winningest_coach(season_id)
+    teams = ids_with_game_arrays_by_season(season_id)
+    teams.each do |team_id, games|
+      if games.length == 0
+        teams.delete(team_id)
+      end
+    end
+
+    #Game CSV has season, team ids (split home/away), game_id
+    #Game Team CSV has team_id and head coach names
+
+    teams.each do |team_id, games|
+      season_games = 0
+      season_wins = 0
+      games.each do |game|
+        season_games += 1
+        if game.home_team_id == team_id && game.outcome.include?("home")
+          season_wins += 1
+        elsif game.away_team_id == team_id && game.outcome.include?("away")
+          season_wins += 1
+        end
+      end
+      teams[team_id] = (season_wins/season_games.to_f)
+    end
+
+    best_team = teams.max_by do |team_id, ratio|
+      ratio
+    end.first
+
+    self.game_teams.find do |game|
+      game.game_id[0..3] == season_id[0..3] && game.team_id == best_team
+    end.head_coach
+
   end
 end
